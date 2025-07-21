434fc30e15f51b5dae3a555bacf7f67e57a0ad29
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-07-21 08:27:16 +0000

    [finish] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get '/homes/about'  => 'homes#About', as: 'about'
+  root to: 'homes#top'
   devise_for :users
-  root to: "homes#top"
+  
+  #get 'post_images/new' get 'post_images/index' get 'post_images/show'の短縮表記、resourcesの一括生成機能の範囲を限定して実現している
+  #:createを追加（10章）
+  resources :post_images, only: [:new, :create, :index, :show]
+
+  get "/homes/about" => "homes#about", as: "about"
 end
